ae958c96ce29217294dc5a098236645fdeb6ae31
phongtt <phongtt.international@gmail.com>
2020-05-10 00:12:18 -0700

    Migrations Database

--- src/KnowledgeSpace.BackendServer/Data/Entities/Report.cs
+++ src/KnowledgeSpace.BackendServer/Data/Entities/Report.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using KnowledgeSpace.BackendServer.Data.Interface;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KnowledgeSpace.BackendServer.Data.Entities
 {
-    public class Report
+    [Table("Reports")]
+    public class Report : IDateTracking
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? KnowledgeBaseId { get; set; }
+
+        public int? CommentId { get; set; }
+
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [MaxLength(50)]
+        [Column(TypeName = "varchar(50)")]
+        public string ReportUserId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        [MaxLength(50)]
+        [Column(TypeName = "varchar(50)")]
+        public string Type { get; set; }
     }
 }
