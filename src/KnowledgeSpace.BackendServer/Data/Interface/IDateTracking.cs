1757def1b3a801f47a268d7227de4e42663eedf9
phongtt <phongtt.international@gmail.com>
2020-05-09 23:33:45 -0700

    Create Entity class

---
+++ src/KnowledgeSpace.BackendServer/Data/Interface/IDateTracking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.BackendServer.Data.Interface
+{
+    public interface IDateTracking
+    {
+        DateTime CreateDate { get; set; }
+        DateTime? LastModifiedDate { get; set; }
+    }
+}
